3493de6c778a17f28a78188f3d3eda5a0bddfafd
Hassan Rehan <hrehan752@gmail.com>
2025-03-16 01:23:22 +0500

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import contectDB from './config/db'
-import auth from './routes/auth'
-import prop from './routes/prop'
+import connectDB from './config/db';
+import auth from './routes/auth';
+import prop from './routes/prop';
 import http from 'http';
 import setupSocket from './sockets/sockets';
 
@@ -25,8 +25,8 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', auth);
 app.use('/api/prop', prop);
 
-contectDB().then(() => {
+connectDB().then(() => {
   server.listen(process.env.PORT || 5200, () => {
-    console.log(`Server running`);
+    console.log(`Server running on port ${process.env.PORT || 5200}`);
   });
 });
