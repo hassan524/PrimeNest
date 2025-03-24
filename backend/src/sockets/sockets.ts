7b7af38b626815db1b91e958d4ca264f8bbf1d11
Hassan Rehan <hrehan752@gmail.com>
2025-03-24 23:37:10 +0500

    Update sockets.ts

--- backend/src/sockets/sockets.ts
+++ backend/src/sockets/sockets.ts
@@ -6,7 +6,7 @@ const db = admin.firestore();
 const setupSocket = (server: any) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000/",
+      origin: "http://localhost:3000",
       methods: ["GET", "POST"],
       credentials: true,
     },
