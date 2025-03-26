3e66e28f567ce53ed8f8d9c8978508251a77c480
Hassan Rehan <hrehan752@gmail.com>
2025-03-26 16:07:48 +0500

    Update page.tsx

--- frontend/src/app/messages/[userid]/page.tsx
+++ frontend/src/app/messages/[userid]/page.tsx
@@ -69,7 +69,8 @@ export default function UserMessages() {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
       {/* Messages Display */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-3">
+      <div className="flex-1 overflow-y-auto sm:p-4 p-3 space-y-3"
+        style={{ scrollbarWidth: "none" }}>
         {messages.map((msg, index) => (
           <div
             key={msg.id || `${msg.from}-${index}`} 
