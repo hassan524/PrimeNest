759a83a5c788213bac8716ed8648d0d03733f52f
Hassan Rehan <your-email@example.com>
2025-03-28 00:57:54 +0500

    fixed a bug

--- frontend/src/components/ui/ListingChart.tsx
+++ frontend/src/components/ui/ListingChart.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Bar, BarChart, XAxis } from "recharts"
+import { Bar, BarChart, XAxis, ResponsiveContainer } from "recharts";
 
 import {
   ChartConfig,
@@ -9,45 +9,46 @@ import {
   ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart"
+} from "@/components/ui/chart";
 
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-]
+  { month: "January", ForSale: 186, ForRent: 80 },
+  { month: "February", ForSale: 305, ForRent: 200 },
+  { month: "March", ForSale: 237, ForRent: 120 },
+  { month: "April", ForSale: 73, ForRent: 190 },
+  { month: "May", ForSale: 209, ForRent: 130 },
+  { month: "June", ForSale: 214, ForRent: 140 },
+];
 
 const chartConfig = {
   ForSale: {
     label: "ForSale",
-    color: "#2563eb",
+    color: "#2563eb", 
   },
   ForRent: {
     label: "ForRent",
-    color: "#60a5fa",
+    color: "#60a5fa", 
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
 export function ListingsTrendChart() {
   return (
     <ChartContainer config={chartConfig} className="h-[50vh] w-full bg-white">
-      <BarChart accessibilityLayer data={chartData}>
-        {/* <CartesianGrid vertical={false} />  */}
-        <XAxis
-          dataKey="month"
-          tickLine={false}
-          tickMargin={10}
-          axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
-        />
-        <ChartTooltip content={<ChartTooltipContent />} />
-        <ChartLegend content={<ChartLegendContent />} />
-        <Bar dataKey="ForSale" fill="var(--color-desktop)" radius={4} />
-        <Bar dataKey="ForRent" fill="var(--color-mobile)" radius={4} />
-      </BarChart>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={chartData}>
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey="ForSale" fill={chartConfig.ForSale.color} radius={4} />
+          <Bar dataKey="ForRent" fill={chartConfig.ForRent.color} radius={4} />
+        </BarChart>
+      </ResponsiveContainer>
     </ChartContainer>
-  )
+  );
 }
